75a48033d2cabf1dac4ff994f0720d4826becbd5
Wellington Lima <Wellington.360@hotmail.com>
2022-11-11 20:50:43 -0300

    first commit

---
+++ TesteParte1/Conexao.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexao
+{
+    internal class CriarConexao
+    {
+        public void saveUsers(string Nome, DateTime DataDeCriacao)
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;port=3306;database=phpmyadmin;user id=root;pwd=;sslmode=none;");
+            string sqlQuery = "INSERT INTO users (Nome, DataCriacao) Values('" + Nome + "', '" + DataDeCriacao + "')";
+            MySqlCommand command = new MySqlCommand(sqlQuery);
+            command.Connection = conn;
+
+            try
+            {
+                conn.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                command.Connection.Close();
+            }
+
+        }
+    }
+}
---
+++ TesteParte1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Conexao;
+using MySql.Data.MySqlClient;
+using System;
+using System.Globalization;
+
+namespace C_
+
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataCriada { get; set; }
+    }
+    public class Teste
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            var data = new DateTime(2008, 5, 1);
+            List<User> users = new List<User>
+            {
+                new User {Id=1, Nome="Well",    DataCriada=dateTime},
+                new User {Id=2, Nome="Luan",    DataCriada=dateTime},
+                new User {Id=3, Nome="Luã",     DataCriada=dateTime},
+                new User {Id=4, Nome="João",    DataCriada=dateTime},
+                new User {Id=5, Nome="Augusto", DataCriada=dateTime},
+                new User {Id=6, Nome="Cláudio", DataCriada=dateTime},
+                new User {Id=7, Nome="Gabriela",DataCriada=dateTime},
+                new User {Id=8, Nome="Joyce",   DataCriada=dateTime},
+                new User {Id=9, Nome="Mitchel", DataCriada=dateTime},
+                new User {Id=10,Nome="Luiz",    DataCriada=dateTime}
+            };
+
+            CriarConexao database = new CriarConexao();
+            foreach (var user in users)
+            {
+                database.saveUsers(user.Nome, user.DataCriada);
+            }
+        }
+    }
+}
